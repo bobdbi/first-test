3cb4d36b390311146d6ea82319d73b0d2e578aee
bobdbi <bob@dbi-net.dk>
2016-03-14 13:14:24 +0100

    third commit

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -49,7 +49,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 18);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Label";
+            this.label1.Text = "Min nye label";
             // 
             // Form1
             // 
